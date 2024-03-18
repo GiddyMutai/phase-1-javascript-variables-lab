c4a08a709f3235ed07c6302900aff7f9db6cbac8
Gideon Mutai <giddymutai13@gmail.com>
2024-03-18 22:03:58 +0300

    Declares variables and assigns values to them

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Code your solution in this file!
+// This declares and assigns a value to the variable.
+const companyName = 'Scuber' 
+
+let mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo = 'Susan Smith'
